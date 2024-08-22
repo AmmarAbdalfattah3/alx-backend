cd24b715dbf14db2e1522c74ea8c8d4180c9eadd
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-22 01:49:12 +0000

    LRU cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,34 +3,41 @@
 """
 
 
-from base_caching import BaseCaching
 from collections import OrderedDict
-
+from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
-    """LRUCache class inherits from BaseCaching and implements a LRU cache.
     """
+    LRUCache class that inherits from BaseCaching and implements
+    a Least Recently Used (LRU) caching system.
+    """
+
     def __init__(self):
-        """Initialize the LRUCache
+        """
+        Initialize the LRUCache.
         """
         super().__init__()
-        self.cache_data = OrderDict()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Add an item to the cache.
+        """
+        Add or update an item in the cache.
         """
         if key is None or item is None:
-            return None
+            return
         if key in self.cache_data:
-            self.cache_data.move_to_end(key, last: True)
-        if len(self.cache_data) >= super().MAX_ITEMS:
-            oldest_key = self.cache_data.popitem(last=False)
+            self.cache_data.move_to_end(key)
         self.cache_data[key] = item
-        print("DISCARD: {}".format(oldest_key))
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {oldest_key}")
 
     def get(self, key):
-        """Retrieve an item from the cache.
         """
-        if key and key in self.cache_data:
-            self.cache_data.move_to_end(key, last: True)
-        return self.cache_data.get(key, None)
+        Retrieve an item from the cache.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
