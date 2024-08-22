defc8fc1374aed0746969f5ac3da9af8d73f83eb
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-22 02:45:28 +0000

    LFU cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -6,34 +6,57 @@
 from collections import defaultdict, OrderedDict
 from base_caching import BaseCaching
 
-
 class LFUCache(BaseCaching):
+    """LFUCache class inherits from BaseCaching and implements a Least Frequently Used (LFU) cache."""
+
     def __init__(self):
         """Initialize the LFUCache."""
         super().__init__()
         self.cache_data = {}
-        self.freq = defaultdict(int)
-        self.order = defaultdict(OrderedDict)
-        self.min_freq = 1
+        self.freq = defaultdict(int)  # To track the frequency of access
+        self.order = defaultdict(OrderedDict)  # To track the order of access per frequency level
+        self.min_freq = 1  # To track the minimum frequency
 
     def put(self, key, item):
-        """Add an item to the cache."""
+        """Add an item to the cache.
+
+        If the cache exceeds BaseCaching.MAX_ITEMS, discard the least frequently used item.
+        If multiple items have the same frequency, discard the least recently used one.
+
+        Args:
+            key (str): The key to store the item under.
+            item (str): The value to store in the cache.
+
+        If `key` or `item` is None, this method does nothing.
+        """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
+            # Update the existing item
             self._update_frequency(key)
             self.cache_data[key] = item
         else:
+            # Add a new item
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 self._evict()
+
             self.cache_data[key] = item
             self.freq[key] = 1
             self.order[1][key] = None
             self.min_freq = 1
 
     def get(self, key):
-        """Retrieve an item from the cache."""
+        """Retrieve an item from the cache.
+
+        Updates the frequency and order of the item.
+
+        Args:
+            key (str): The key of the item to retrieve.
+
+        Returns:
+            str: The value associated with the key.
+        """
         if key is None or key not in self.cache_data:
             return None
 
@@ -41,19 +64,25 @@ class LFUCache(BaseCaching):
         return self.cache_data[key]
 
     def _update_frequency(self, key):
-        """Update the frequency and access order of a key."""
+        """Update the frequency and order of the given key."""
         freq = self.freq[key]
+        # Remove the key from the current frequency level
         del self.order[freq][key]
         if not self.order[freq]:
             del self.order[freq]
             if self.min_freq == freq:
                 self.min_freq += 1
+
+        # Update frequency and add to the new frequency level
         self.freq[key] += 1
-        self.order[self.freq[key]][key] = None
+        new_freq = self.freq[key]
+        self.order[new_freq][key] = None
 
     def _evict(self):
-        """Evict the least frequently used item, using LRU if necessary."""
+        """Evict the least frequently used item."""
+        # Get the least frequently used level
         lfu_keys = self.order[self.min_freq]
+        # Remove the least recently used item from the least frequent level
         lru_key, _ = lfu_keys.popitem(last=False)
         del self.cache_data[lru_key]
         del self.freq[lru_key]
@@ -65,3 +94,4 @@ class LFUCache(BaseCaching):
         print("Current cache:")
         for key, value in self.cache_data.items():
             print(f"{key}: {value}")
+
