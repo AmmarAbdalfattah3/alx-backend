35d4e021e7854c8cedf22e98632e74d63656b824
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-28 00:04:23 +0000

    Basic Flask App

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""flask module
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Handle requests to the root URL ("/").
+    """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    """
+    The script will run the Flask development server if the script is 
+    executed directly (i.e., not imported as a module).
+    """
+    app.run(debug=True)
